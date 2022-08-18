833674346cf5ea820f06630e7771a31371d32ca9
Olivier Lopes Rubinger <90404170+olivierrubinger@users.noreply.github.com>
2022-08-18 19:26:38 -0300

    Update Projeto.java

--- codigo/projeto/Projeto.java
+++ codigo/projeto/Projeto.java
@@ -3,8 +3,11 @@ package projeto;
 public class Projeto {
     public static void main(String[] args) {
         Compromisso c = new Compromisso("Pilates", "20/03/2019", 7, 15);
+        Compromisso c2 = new Compromisso("Pelada", "12/05/2021", 14, 8);
         //System.out.println(c.data.dataFormatada());
         c.relatorioDatas();
+        System.out.println("-----------------------------------------------------------------------------------------");
+        c2.relatorioDatas();
     }
 
 }
