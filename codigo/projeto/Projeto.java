c54359d7399453c4612a0b906440cef6ab4bceb2
Olivier Lopes Rubinger <90404170+olivierrubinger@users.noreply.github.com>
2022-08-25 22:21:29 -0300

    Update Projeto.java

--- codigo/projeto/Projeto.java
+++ codigo/projeto/Projeto.java
@@ -7,9 +7,9 @@ public class Projeto {
         Compromisso c = new Compromisso("Pilates", "02/05/2020", 7, 4);
         Compromisso c2 = new Compromisso("Pelada", "03/05/2020", 4, 4);
         String relatorio = new Calendario().relatorioDatasSelecionadas("02/05/2020","23/05/2020");
-        System.out.println(c.relatorioDatas());
+        System.out.println(c.relatorioCompromissos());
         System.out.println("-----------------------------------------------------------------------------------------");
-        System.out.println(c2.relatorioDatas());
+        System.out.println(c2.relatorioCompromissos());
         System.out.println("-----------------------------------------------------------------------------------------");
         System.out.println(relatorio);
     }
